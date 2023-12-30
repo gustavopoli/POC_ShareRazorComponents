01988a081a5d39b37157ac7ad78982fffb8de6bd
Gustavo Poli <gustavo.poli@outlook.com>
2023-12-30 15:36:37 -0800

    v0 Project
    Create and publishing projects

---
+++ HelloMauiByBlazor/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloMauiByBlazor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
